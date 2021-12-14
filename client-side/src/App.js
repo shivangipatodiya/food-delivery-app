2b3ac9514d491103679c718898e3dba041d2a7ce
Shivangi Patodiya <shivu2610@gmail.com>
2021-12-13 18:09:03 -0700

    create login form

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Login from './components/Login'
-import Register from './components/Register'
 
 function App() {
   return (
     <div className="App">
       <Login />
-      <Register />
     </div>
   );
 }
--- client-side/src/components/Login.jsx
+++ client-side/src/components/Login.jsx
@@ -1,18 +1,34 @@
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button"
+import "./login.scss";
+
 export default function Login() {
   return (
-    <div>
-      <h2>Login</h2>
-      <form>
-        <label>
-          Email:
-          <input type="email" name="email" />
-        </label>
-        <label>
-          Password:
-          <input type="password" name="password" />
-        </label>
-        <button type="submit">Login</button>
-      </form>
+    <div className="login">
+      <h2 className="mb-3">Login</h2>
+      <FloatingLabel controlId="floatingInput" label="Email" className="mb-3 login-input">
+        <Form.Control type="email" placeholder="name@example.com" />
+      </FloatingLabel>
+      <FloatingLabel className="login-input" controlId="floatingPassword" label="Password">
+        <Form.Control type="password" placeholder="Password" />
+      </FloatingLabel>
+      <br />
+      <Button className="login-input mb-2" variant="primary">Login</Button>
+      <Button className="login-input mb-2" variant="outline-primary">Signup</Button>
+      <Button variant="link">Forgot Password</Button>
+      <div className="login-seperator">
+        <div style={{width: 140}}>
+          <hr />
+        </div>
+        <div>
+          Login using:
+        </div>
+        <div style={{width: 140}}>
+          <hr />
+        </div>
+      </div>
+      <div class="g-signin2" data-onsuccess="onSignIn"></div>
     </div>
   );
 }
