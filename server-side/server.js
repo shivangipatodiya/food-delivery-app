740ccf03f4fa7d7ea569ec8ab1f8e5b6591f7c2c
Shivangi Patodiya <shivu2610@gmail.com>
2022-02-07 17:17:14 -0700

    refactor database connection

--- server-side/server.js
+++ server-side/server.js
@@ -1,28 +1,45 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3546;
 
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    extended: true,
+    extended: true
   })
-)
+);
 
-const { Pool } = require("pg");
-const dbParams = require("./db.js");
-const db = new Pool(dbParams);
-db.connect();
+const { Sequelize } = require("sequelize");
 
+const sequelize = new Sequelize("fooddelivery", "owner", "owner", {
+  host: "localhost",
+  dialect: "postgres"
+});
 
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
-app.get('/', (req, res) => {
-  res.send('Hungry! Order some food...')
-})
+// sequelize
+//   .authenticate()
+//   .then(() => console.log("Connection has been established successfully."))
+//   .catch((error) => console.error("Unable to connect to the database:", error));
+
+// const { Pool } = require("pg");
+// const dbParams = require("./db.js");
+// const db = new Pool(dbParams);
+// db.connect();
+
+app.get("/", (req, res) => {
+  res.send("Hungry! Order some food...");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
