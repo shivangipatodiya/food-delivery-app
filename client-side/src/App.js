83942b73d2820250ae45ba6c5c2889ddad2fd7fa
Shivangi Patodiya <shivu2610@gmail.com>
2022-02-14 17:37:14 -0700

    create private route

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -2,27 +2,28 @@ import "./App.css";
 import Login from "./components/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
+import PrivateRoute from "./PrivateRoute";
 import Home from "./components/Home";
-import { useEffect, useState } from "react";
-import axios from "axios";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
 
 function App() {
-  const [user, setUser] = useState({});
+  // const [user, setUser] = useState({});
 
-  const fetchUser = async () => {
-    const userData = await axios.get("/api/user")
-  }
+  // const fetchUser = async () => {
+  //   const userData = await axios.get("/api/user")
+  // }
 
-  useEffect(() => {
-    fetchUser()
-  }, [fetchUser])
+  // useEffect(() => {
+  //   fetchUser()
+  // }, [fetchUser])
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
         </Routes>
       </BrowserRouter>
     </>
